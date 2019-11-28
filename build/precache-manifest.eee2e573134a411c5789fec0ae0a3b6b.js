a4683fe6f88bc898abd4d8ccdda9a288cffd8e7f
xml123 <xumingliang@fang88.me>
2019-11-28 11:15:25 +0800

    add build

---
+++ build/precache-manifest.eee2e573134a411c5789fec0ae0a3b6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47ca4302a399de0147cb61daaa0b0ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5a9db5bc240314a01fc",
+    "url": "/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "079670ebd2f058fe2753",
+    "url": "/static/css/main.7960e558.chunk.css"
+  },
+  {
+    "revision": "c5a9db5bc240314a01fc",
+    "url": "/static/js/2.4ef2ba99.chunk.js"
+  },
+  {
+    "revision": "079670ebd2f058fe2753",
+    "url": "/static/js/main.34b0913e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bdb85e2095845381a285ab7f1ab710ac",
+    "url": "/static/media/ruanyifeng.bdb85e20.png"
+  }
+]);
